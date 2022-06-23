1798e3925bc8e4b9d0da08e87b8c0e5fb9e55fa2
MaaxIT42 <contact@maaxit.fr>
2022-06-23 18:59:02 +0200

    Make header comments norm-friendly

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbennafl <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mpeharpr <mpeharpr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 14:53:19 by mbennafl          #+#    #+#             */
-/*   Updated: 2022/06/23 16:50:25 by mbennafl         ###   ########.fr       */
+/*   Updated: 2022/06/23 18:58:00 by mpeharpr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,19 @@
 # include <errno.h>
 # include <readline/readline.h>
 
-// Builtins	functions declarations
-
+/* Builtins	prototypes */
 int	bi_echo(int fd, int newline, char *str);
 int	bi_cd(int fd, char *str);
 int	bi_pwd(int fd);
 
-// Redirections functions declarations
-
+/* Redirections prototypes */
 int	rd_output(char *path);
 int	rd_output_append(char *path);
 
-// Utils	functions declarations
-
+/* Utils prototypes */
 int	ft_putstr_fd(int fd, char *str);
 
-// Errors	functions declarations
-
+/* Errors prototypes */
 int	print_error(int ret);
 
 #endif
