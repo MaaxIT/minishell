8a3e780e78c8d7d90ac892b941ad2260900f01c9
MaaxIT <contact@maaxit.fr>
2022-06-28 01:23:35 +0200

    Create the actual parsing system

---
+++ src/parsing.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpeharpr <mpeharpr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/27 19:01:52 by mpeharpr          #+#    #+#             */
+/*   Updated: 2022/06/28 01:01:48 by mpeharpr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/* Properly free a command */
+void    free_command(t_command *cmd)
+{
+    free(cmd->full);
+    free(cmd);
+}
+
+/* Initialize the command with values we got */
+static void init_command(t_command *new, char **splitted)
+{
+    int argc;
+
+    argc = 0;
+    while (splitted[argc] && *splitted[argc])
+        argc++;
+    if (argc > 0)
+       argc--;
+    new->argc = argc;
+    new->full = splitted;
+    new->bin = splitted[0];
+    if (splitted[1])
+        new->argv = &splitted[1];
+    else
+        new->argv = NULL;
+}
+
+/* Split the line for every space */
+t_command   *parse_cmd(char *cmd)
+{
+    char        **splitted;
+    t_command   *new;
+
+    new = malloc(sizeof(t_command));
+    if (!new)
+        return (NULL);
+    new->full = NULL;
+    new->bin = NULL;
+    new->argv = NULL;
+    new->argc = -1;
+    splitted = split_command(cmd);
+    if (!splitted || !splitted[0])
+    {
+        free(new);
+        if (splitted != NULL)
+            free(splitted);
+        return (NULL);
+    }
+    init_command(new, splitted);
+    return (new);
+}
---
+++ src/split.c
@@ -0,0 +1,131 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpeharpr <mpeharpr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/27 23:11:22 by mpeharpr          #+#    #+#             */
+/*   Updated: 2022/06/28 01:20:31 by mpeharpr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/* Make sure that there is an even amount of single & doubles quotes in
+the command line - this way, we can make sure all quotes are closing and
+we don't have any unclosed quote as specified in the subject */
+static int  quotes_even(char *cmd)
+{
+    int i;
+    int simples;
+    int doubles;
+
+    simples = 0;
+    doubles = 0;
+    i = 0;
+    while (cmd[i])
+    {
+        if (cmd[i] == '\'')
+            simples++;
+        else if (cmd[i] == '\"')
+            doubles++;
+        i++;
+    }
+    return (simples % 2 == 0 && doubles % 2 == 0);
+}
+
+/* Count the amount of splits we will have in order to pre-allocate */
+static int  count_splits(char *cmd)
+{
+    int     i;
+    int     idx;
+    int     capt;
+
+    idx = 0;
+    capt = 0;
+    i = 0;
+    while (cmd[i])
+    {
+        if ((cmd[i] == '\'' || cmd[i] == '\"') && cmd[i + 1])
+            capt = !capt;
+        else if (cmd[i] == ' ' && !capt)
+            idx++;
+        i++;
+    }
+    if (i > 0)
+        idx++;
+    return (idx);
+}
+
+/* Split the str with spaces + simple & double quotes */
+static int  split_with_quotes(char *cmd, char **arr)
+{
+    int     i;
+    int     idx;
+    int     capt;
+    int     last;
+
+    idx = 0;
+    capt = 0;
+    last = 0;
+    i = 0;
+    while (cmd[i])
+    {
+        if ((cmd[i] == '\'' || cmd[i] == '\"') && cmd[i + 1])
+            capt = !capt;
+        else if (cmd[i] == ' ' && !capt)
+        {
+            arr[idx] = ft_substr(cmd, last, i - last);
+            if (!arr[idx])
+                return (-1); // Memory error
+            idx++;
+            last = i + 1;
+        }
+        i++;
+    }
+    if (i > last)
+    {
+        arr[idx] = ft_substr(cmd, last, i - last);
+        if (!arr[idx])
+            return (-1); // Memory error
+    }
+    return (0);
+}
+
+/* Free the split and return NULL in case of an error */
+static char **free_split(char **arr)
+{
+    int idx;
+
+    idx = 0;
+    while (arr[idx])
+        free(arr[idx++]);
+    free(arr);
+    return (NULL); // just to win lines from the norminette by returning the function (see below)
+}
+
+/* The main function that is splitting the command line and returning a 2D array */
+char    **split_command(char *cmd)
+{
+    int     splitc;
+    char    **splitv;
+    int     idx;
+
+    if (!cmd)
+        return (NULL); // In case cmd is invalid
+    if (!quotes_even(cmd))
+        return (NULL); // In case quotes are not closed
+    splitc = count_splits(cmd);
+    if (splitc <= 0)
+        return (NULL); // In case cmd is empty
+    splitv = malloc(sizeof(char *) * (splitc + 1));
+    if (!splitv)
+        return (NULL); // In case of memory error
+    idx = 0;
+    while (idx <= splitc)
+        splitv[idx++] = NULL;
+    if (split_with_quotes(cmd, splitv) == -1)
+        return (free_split(splitv));
+    return (splitv);
+}
