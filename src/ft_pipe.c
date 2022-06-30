7213c2f38abf9b3244f281e1b13a3489f4a6d9df
b <b>
2022-06-30 23:05:45 +0200

    pipe

--- src/ft_pipe.c
+++ src/ft_pipe.c
@@ -2,6 +2,8 @@
 
 int	first_cmd(int pipefd[2], char **argv)
 {
+(void)argv;
+
 	if (close(pipefd[0]) == -1)
 		return (0);
 	if (dup2(pipefd[1], 1) == -1)
@@ -12,6 +14,8 @@ int	first_cmd(int pipefd[2], char **argv)
 
 int	second_cmd(int pipefd[2], char **argv)
 {
+(void)argv;
+
 	if (close(pipefd[1]) == -1)
 		return (0);
 	if (dup2(pipefd[0], 0) == -1)
@@ -29,9 +33,11 @@ int	ft_pipe(char **argv1, char **argv2)
 	if (pipe(pipefd) == -1)
 		return (0); // ENOUGH?
 	stdin_fd = dup(1);
+	pid = fork();
 	if (pid != 0 && first_cmd(pipefd, argv1) == 0)
 		return (0);
 	else if (pid == 0 && second_cmd(pipefd, argv2) == 0)
 		return (0);
+	dup2(stdin_fd, 1);
 	return (9);
 }
