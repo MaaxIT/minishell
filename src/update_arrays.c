74b30e1e7c3a545d4f8c4fcad025ce0f14fe8e9c
b <b>
2022-07-16 00:23:30 +0200

    update

--- src/update_arrays.c
+++ src/update_arrays.c
@@ -25,11 +25,17 @@ static char	**ft_pop_null(char **tab, int *tabsize)
 
 int	update_inputv_optionsv_after_redir(t_cmd_lst *cmd)
 {
-	cmd->options_v = ft_pop_null(cmd->options_v, &cmd->options_c);
-	if (!cmd->options_v)
-		return (0);
-	cmd->input_v = ft_pop_null(cmd->input_v, &cmd->input_c);
-	if (!cmd->input_v)
-		return (0);
+	if (cmd->options_c)
+	{
+		cmd->options_v = ft_pop_null(cmd->options_v, &cmd->options_c);
+		if (!cmd->options_v)
+			return (0);
+	}
+	if (cmd->input_c)
+	{
+		cmd->input_v = ft_pop_null(cmd->input_v, &cmd->input_c);
+		if (!cmd->input_v)
+			return (0);
+	}
 	return (9);
 }
