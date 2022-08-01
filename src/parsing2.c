45b87ad9898c8170d1fb2e733e8b4e987129ab42
MaaxIT <contact@maaxit.fr>
2022-08-01 04:55:37 +0200

    Fucking norm for parsing2

--- src/parsing2.c
+++ src/parsing2.c
@@ -6,7 +6,7 @@
 /*   By: maxime <maxime@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 21:47:17 by mpeharpr          #+#    #+#             */
-/*   Updated: 2022/08/01 03:09:48 by maxime           ###   ########.fr       */
+/*   Updated: 2022/08/01 04:55:14 by maxime           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,15 @@ int	get_input_idx(t_cmd_lst *cmd_t, char *str)
 	}
 	return (-1);
 }
+
+
+
+
+
+
+
+
+
 
 /* Remove a value from the options_v */
 void	remove_from_options(t_cmd_lst *cmd_t, char *addr, int *cnt)
@@ -78,6 +87,46 @@ int	rem_from_both(t_cmd_lst *cmd_t, char *addr)
 	return (0);
 }
 
+
+
+
+
+
+
+
+
+
+
+/* Remove an empty argument from arrays while synchronizing */
+int	sync_arg_empty(t_cmd_lst *cmd_t, int idx, int input_idx, int is_bin)
+{
+	if (cmd_t->parsing_v && cmd_t->parsing_v[idx])
+	{
+		cmd_t->parsing_v = ft_pop(cmd_t->parsing_v, idx, cmd_t->arg_c);
+		if (!cmd_t->parsing_v)
+			return (-1);
+	}
+	cmd_t->arg_v = ft_pop(cmd_t->arg_v, idx, cmd_t->arg_c--);
+	if (!cmd_t->arg_v)
+		return (-1);
+	if (input_idx >= 0)
+		cmd_t->input_v[input_idx] = NULL;
+	if (is_bin)
+	{
+		if (cmd_t->arg_c > 0)
+		{
+			cmd_t->binary = cmd_t->arg_v[0];
+			if (rem_from_both(cmd_t, cmd_t->arg_v[0]) == -1)
+				return (-1);
+		}
+		else
+			cmd_t->binary = NULL;
+	}
+	if (update_inputv_optionsv_after_redir(cmd_t) == -1)
+		return (-1);
+	return (0);
+}
+
 /* Synchronize arguments between each array of the structure */
 int	sync_arg(t_cmd_lst *cmd_t, char *old_input, char *new_input)
 {
@@ -98,36 +147,165 @@ int	sync_arg(t_cmd_lst *cmd_t, char *old_input, char *new_input)
 				cmd_t->binary = cmd_t->arg_v[idx];
 			else if (input_idx >= 0)
 				cmd_t->input_v[input_idx] = cmd_t->arg_v[idx];
-			if (ft_strlen(new_input) == 0)
-			{
-				if (cmd_t->parsing_v && cmd_t->parsing_v[idx])
-				{
-					cmd_t->parsing_v = ft_pop(cmd_t->parsing_v, idx, cmd_t->arg_c);
-					if (!cmd_t->parsing_v)
-						return (-1);
-				}
-				cmd_t->arg_v = ft_pop(cmd_t->arg_v, idx, cmd_t->arg_c--);
-				if (!cmd_t->arg_v)
-					return (-1);
-				if (input_idx >= 0)
-					cmd_t->input_v[input_idx] = NULL;
-				if (is_bin)
-				{
-					if (cmd_t->arg_c > 0)
-					{
-						cmd_t->binary = cmd_t->arg_v[0];
-						if (rem_from_both(cmd_t, cmd_t->arg_v[0]) == -1)
-							return (-1);
-					}
-					else
-						cmd_t->binary = NULL;
-				}
-				if (update_inputv_optionsv_after_redir(cmd_t) == -1)
-					return (-1);
-			}
+			if (ft_strlen(new_input) == 0 && \
+			sync_arg_empty(cmd_t, idx, input_idx, is_bin) == -1)
+				return (-1);
 			break ;
 		}
 		idx++;
+	}
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+/* Parse redirection that are next to the redirection character 
+returns:
+	- 0 = continue
+	- -1 = memory error
+*/
+int	parse_redirections_concatenated(t_cmd_lst *cmd_t, char **path_type, int *idx, int *i)
+{
+	int		k;
+	int		fd;
+	int		len;
+
+	len = *idx + 1;
+	while (cmd_t->arg_v[*i][len] && cmd_t->arg_v[*i][len] != '>' && \
+	cmd_t->arg_v[*i][len] != '<')
+		len++;
+	*path_type = ft_strndup(cmd_t->arg_v[*i] + *idx + 1, len - (*idx + 1));
+	if (!*path_type)
+		return (-1);
+	if (cmd_t->output_type == 'A' && cmd_t->input_path)
+	{
+		rd_delimiter(cmd_t->input_path);
+		free(*path_type);
+		*path_type = NULL;
+		cmd_t->input_path = NULL;
+	}
+	else
+	{
+		fd = open(*path_type, O_CREAT, 0644);
+		if (fd >= 0)
+			close(fd);
+	}
+	if (cmd_t->output_type == 'A')
+	{
+		k = *idx - 1;
+		while (k < len)
+		{
+			if (remove_char_from_str(cmd_t, &cmd_t->arg_v[*i], *idx - 1) == 1)
+			{
+				(*i)--;
+				break ;
+			}
+			k++;
+		}
+	}
+	else
+	{
+		k = *idx;
+		while (k < len)
+		{
+			if (remove_char_from_str(cmd_t, &cmd_t->arg_v[*i], *idx) == 1)
+			{
+				(*i)--;
+				break ;
+			}
+			k++;
+		}
+	}
+	*idx = 0;
+	return (0);
+}
+
+int	parse_redirections_separated(t_cmd_lst *cmd_t, char **path_type, int *i)
+{
+	int	fd;
+	int	input_idx;
+
+	if (remove_char_from_str(cmd_t, &cmd_t->arg_v[*i], ft_strlen(cmd_t->arg_v[*i]) - 1) == 1)
+		(*i)--;
+	if (cmd_t->output_type == 'A' && remove_char_from_str(cmd_t, &cmd_t->arg_v[*i], ft_strlen(cmd_t->arg_v[*i]) - 1) == 1)
+		(*i)--;
+	input_idx = 0;
+	while (cmd_t->arg_v[*i + 1][input_idx] && cmd_t->arg_v[*i + 1][input_idx] != '>' && cmd_t->arg_v[*i + 1][input_idx] != '<')
+		(input_idx)++;
+	*path_type = ft_strndup(cmd_t->arg_v[*i + 1], input_idx);
+	if (!*path_type)
+		return (-1);
+	if (cmd_t->output_type == 'A' && cmd_t->input_path)
+		rd_delimiter(cmd_t->input_path);
+	else
+	{
+		fd = open(*path_type, O_CREAT, 0644);
+		if (fd >= 0)
+			close(fd);
+	}
+	replace_sub(cmd_t, &cmd_t->arg_v[*i + 1], *path_type, "");
+	if (cmd_t->output_type == 'A' && cmd_t->input_path)
+	{
+		free(*path_type);
+		*path_type = NULL;
+		cmd_t->input_path = NULL;
+	}
+	return (0);
+}
+
+/* Callback for every looped character 
+returns:
+	- -1 = memory error
+	- 0 = do nothing
+	- 1 = continue
+*/
+int	parse_redirections_loop(t_cmd_lst *cmd_t, char **path_type, int *idx, int *i)
+{
+	int		good;
+	char	c;
+
+	c = '>';
+	if (path_type == &cmd_t->input_path)
+		c = '<';
+	if (cmd_t->parsing_v[*i][*idx] == 'S' || cmd_t->parsing_v[*i][*idx] == 'D')
+	{
+		(*idx)++;
+		return (1);
+	}
+	if (cmd_t->arg_v[*i][*idx] == c)
+	{
+		if (cmd_t->arg_v[*i][*idx + 1] == c)
+		{
+			cmd_t->output_type = 'A';
+			(*idx)++;
+		}
+		else
+			cmd_t->output_type = 'R';
+		good = 1;
+		if (cmd_t->arg_v[*i][*idx + 1])
+		{
+			if (parse_redirections_concatenated(cmd_t, path_type, idx, i) == -1)
+				return (-1);
+		}
+		else if (cmd_t->arg_v[*i + 1])
+		{
+			if (parse_redirections_separated(cmd_t, path_type, i) == -1)
+				return (-1);
+		}
+		else
+			good = 0;
+		if (good == 1)
+		{
+			if (*i < 0)
+				*i = 0;
+			return (1);
+		}
 	}
 	return (0);
 }
@@ -137,131 +315,27 @@ int	parse_redirections(t_cmd_lst *cmd_t)
 {
 	int		i;
 	int		j;
-	int		k;
-	int		fd;
 	int		idx;
-	int		len;
-	int		input_idx;
-	char	c;
+	int		rtrn;
 	char	**path_type;
 
 	path_type = &cmd_t->output_path;
-	c = '>';
 	j = 0;
 	while (j < 2)
 	{
 		if (j == 1)
-		{
 			path_type = &cmd_t->input_path;
-			c = '<';
-		}
 		i = 0;
 		while (i < cmd_t->arg_c)
 		{
 			idx = 0;
 			while (cmd_t->arg_v && cmd_t->arg_v[i] && cmd_t->arg_v[i][idx])
 			{
-				if (cmd_t->parsing_v[i][idx] == 'S' || cmd_t->parsing_v[i][idx] == 'D')
-				{
-					idx++;
+				rtrn = parse_redirections_loop(cmd_t, path_type, &idx, &i);
+				if (rtrn == -1)
+					return (-1);
+				else if (rtrn == 1)
 					continue ;
-				}
-				if (cmd_t->arg_v[i][idx] == c)
-				{
-					if (cmd_t->arg_v[i][idx + 1] == c)
-					{
-						cmd_t->output_type = 'A';
-						idx++;
-					}
-					else
-						cmd_t->output_type = 'R';
-					if (cmd_t->arg_v[i][idx + 1])
-					{
-						len = idx + 1;
-						while (cmd_t->arg_v[i][len] && cmd_t->arg_v[i][len] != '>' && \
-						cmd_t->arg_v[i][len] != '<')
-							len++;
-						*path_type = ft_strndup(cmd_t->arg_v[i] + idx + 1, len - (idx + 1));
-						if (!path_type)
-							return (-1);
-						if (cmd_t->output_type == 'A' && cmd_t->input_path)
-						{
-							rd_delimiter(cmd_t->input_path);
-							free(*path_type);
-							*path_type = NULL;
-							cmd_t->input_path = NULL;
-						}
-						else
-						{
-							fd = open(*path_type, O_CREAT, 0644);
-							if (fd >= 0)
-								close(fd);
-						}
-						if (cmd_t->output_type == 'A')
-						{
-							k = idx - 1;
-							while (k < len)
-							{
-								if (remove_char_from_str(cmd_t, &cmd_t->arg_v[i], idx - 1) == 1)
-								{
-									i--;
-									break ;
-								}
-								k++;
-							}
-						}
-						else
-						{
-							k = idx;
-							while (k < len)
-							{
-								if (remove_char_from_str(cmd_t, &cmd_t->arg_v[i], idx) == 1)
-								{
-									i--;
-									break ;
-								}
-								k++;
-							}
-						}
-						idx = 0;
-						if (i < 0)
-							i = 0;
-						continue ;
-					}
-					else if (cmd_t->arg_v[i + 1])
-					{
-						// Parse the char part (the previous argument)
-						if (remove_char_from_str(cmd_t, &cmd_t->arg_v[i], ft_strlen(cmd_t->arg_v[i]) - 1) == 1)
-							i--;
-						if (cmd_t->output_type == 'A' && remove_char_from_str(cmd_t, &cmd_t->arg_v[i], ft_strlen(cmd_t->arg_v[i]) - 1) == 1)
-							i--;
-						// Parse the pathname part
-						input_idx = 0;
-						while (cmd_t->arg_v[i + 1][input_idx] && cmd_t->arg_v[i + 1][input_idx] != '>' && cmd_t->arg_v[i + 1][input_idx] != '<')
-							input_idx++;
-						*path_type = ft_strndup(cmd_t->arg_v[i + 1], input_idx);
-						if (!*path_type)
-							return (-1);
-						if (cmd_t->output_type == 'A' && cmd_t->input_path)
-							rd_delimiter(cmd_t->input_path);
-						else
-						{
-							fd = open(*path_type, O_CREAT, 0644);
-							if (fd >= 0)
-								close(fd);
-						}
-						replace_sub(cmd_t, &cmd_t->arg_v[i + 1], *path_type, "");
-						if (cmd_t->output_type == 'A' && cmd_t->input_path)
-						{
-							free(*path_type);
-							*path_type = NULL;
-							cmd_t->input_path = NULL;
-						}
-						if (i < 0)
-							i = 0;
-						continue ;
-					}
-				}
 				idx++;
 			}
 			i++;
@@ -269,6 +343,50 @@ int	parse_redirections(t_cmd_lst *cmd_t)
 		j++;
 	}
 	return (0);
+}
+
+
+
+
+
+
+
+
+
+/* Parse double quotes, called from parse_input_quotes */
+void	parse_double_quotes(char *input, char *parse, size_t *idx, size_t *i)
+{
+	*idx = *i + 1;
+	while (input[*idx] && input[*idx] != '\"' && parse[*idx] != 'S')
+		(*idx)++;
+	if (*idx != ft_strlen(input))
+		parse[*i] = 'E';
+	*idx = *i + 1;
+	while (input[*idx] && input[*idx] != '\"')
+	{
+		if (input[*idx] != '$')
+			parse[*idx] = 'D';
+		(*idx)++;
+	}
+	if (*idx < ft_strlen(input))
+		parse[*idx] = 'E';
+	*i = *idx;
+}
+
+/* Parse single quotes, called from parse_input_quotes */
+void	parse_single_quotes(char *input, char *parse, size_t *idx, size_t *i)
+{
+	*idx = *i + 1;
+	while (input[*idx] && input[*idx] != '\'' && parse[*idx] != 'S')
+		(*idx)++;
+	if (*idx != ft_strlen(input))
+		parse[*i] = 'E';
+	*idx = *i + 1;
+	while (input[*idx] && input[*idx] != '\'')
+		parse[(*idx)++] = 'S';
+	if (*idx < ft_strlen(input))
+		parse[*idx] = 'E';
+	*i = *idx;
 }
 
 /*
@@ -290,41 +408,164 @@ int	parse_input_quotes(char *input, char *parse)
 	while (input[i])
 	{
 		if (input[i] == '\"' && parse[i] != 'S')
-		{
-			idx = i + 1;
-			while (input[idx] && input[idx] != '\"' && parse[idx] != 'S')
-				idx++;
-			if (idx != ft_strlen(input))
-				parse[i] = 'E';
-			idx = i + 1;
-			while (input[idx] && input[idx] != '\"')
-			{
-				if (input[idx] != '$')
-					parse[idx] = 'D';
-				idx++;
-			}
-			if (idx < ft_strlen(input))
-				parse[idx] = 'E';
-			i = idx;
-		}
+			parse_double_quotes(input, parse, &idx, &i);
 		else if (input[i] == '\'' && parse[i] != 'D')
-		{
-			idx = i + 1;
-			while (input[idx] && input[idx] != '\'' && parse[idx] != 'S')
-				idx++;
-			if (idx != ft_strlen(input))
-				parse[i] = 'E';
-			idx = i + 1;
-			while (input[idx] && input[idx] != '\'')
-				parse[idx++] = 'S';
-			if (idx < ft_strlen(input))
-				parse[idx] = 'E';
-			i = idx;
-		}
+			parse_single_quotes(input, parse, &idx, &i);
 		i++;
 		if (i > ft_strlen(input))
 			i = ft_strlen(input);
 	}
+	return (0);
+}
+
+
+
+/* Initialize the quotes parsing */
+int	parse_quotes_init(t_cmd_lst *cmd_t, int *i)
+{
+	cmd_t->parsing_v = malloc(sizeof(char *) * (cmd_t->arg_c + 1));
+	if (!cmd_t->parsing_v)
+		return (-1);
+	*i = 0;
+	while (*i < cmd_t->arg_c)
+	{
+		cmd_t->parsing_v[*i] = ft_strdup_char('M', ft_strlen(cmd_t->arg_v[*i]));
+		if (!cmd_t->parsing_v[*i])
+			return (-1); // Memory error
+		(*i)++;
+	}
+	cmd_t->parsing_v[*i] = NULL;
+	*i = 0;
+	while (*i < cmd_t->arg_c)
+	{
+		parse_input_quotes(cmd_t->arg_v[*i], cmd_t->parsing_v[*i]);
+		(*i)++;
+	}
+	*i = 0;
+	return (0);
+}
+
+/* Delete quotes marked for deletion from our strings */
+int	parse_quotes_delete(t_cmd_lst *cmd_t, int *i, int *idx)
+{
+	int	rtrn;
+	
+	while (*i < cmd_t->arg_c)
+	{
+		*idx = 0;
+		while (cmd_t->arg_v[*i][*idx])
+		{
+			if (cmd_t->parsing_v[*i][*idx] == 'E')
+			{
+				rtrn = remove_char_from_str(cmd_t, &cmd_t->arg_v[*i], *idx);
+				if (rtrn == -1)
+					return (-1);
+				if (rtrn == 1)
+				{
+					(*i)--;
+					break ;
+				}
+				if (remove_char_from_str(NULL, &cmd_t->parsing_v[*i], *idx) == -1)
+					return (-1);
+			}
+			else
+				(*idx)++;
+		}
+		(*i)++;
+	}
+	*i = 0;
+	return (0);
+}
+
+int	parse_quotes_found_value(t_cmd_lst *cmd_t, char **sub, char *subparse, char *value, int i)
+{
+	int	backup;
+
+	backup = cmd_t->arg_c;
+	if (replace_sub(cmd_t, &cmd_t->arg_v[i], *sub, value) != 0)
+		return (-1);
+	free(*sub);
+	*sub = NULL;
+	if (backup == cmd_t->arg_c)
+	{
+		*sub = ft_strdup_char('M', ft_strlen(value));
+		if (!*sub)
+			return (-1);
+		if (replace_sub(NULL, &cmd_t->parsing_v[i], subparse, *sub) != 0)
+			return (-1);
+		free(*sub);
+		*sub = NULL;
+	}
+	return (0);
+}
+
+int parse_quotes_not_found(t_cmd_lst *cmd_t, char **sub, char *subparse, int i)
+{
+	int	backup;
+
+	backup = cmd_t->arg_c;
+	if (replace_sub(cmd_t, &cmd_t->arg_v[i], *sub, "") != 0)
+		return (-1);
+	free(*sub);
+	*sub = NULL;
+	if (backup == cmd_t->arg_c)
+	{
+		if (replace_sub(NULL, &cmd_t->parsing_v[i], subparse, "") != 0)
+			return (-1);
+	}
+	return (0);
+}
+
+void	parse_quotes_calculate(t_cmd_lst *cmd_t, size_t *len, int *idx, int i)
+{
+	*len = 0;
+	(*idx)++;
+	if (cmd_t->arg_v[i][*idx] != '?')
+	{
+		while (cmd_t->parsing_v[i][*idx] && \
+		(cmd_t->parsing_v[i][*idx] == 'D' || cmd_t->parsing_v[i][*idx] == 'M') && \
+		cmd_t->arg_v[i][*idx] != '$' && cmd_t->arg_v[i][*idx] != '\'' && \
+		cmd_t->arg_v[i][*idx] != '\"' && is_envchar(cmd_t->arg_v[i][*idx], (*len == 0)))
+		{
+			(*len)++;
+			(*idx)++;
+		}
+	}
+	else
+	{
+		*len = 1;
+		(*idx)++;
+	}
+}
+
+int	parse_quotes_loop(t_cmd_lst *cmd_t, t_list *env, int *idx, int i)
+{
+	char	*subparse;
+	size_t	len;
+	t_list	*val;
+	char	*sub;
+
+	parse_quotes_calculate(cmd_t, &len, idx, i);
+	sub = ft_substr(cmd_t->arg_v[i], *idx - len - 1, len + 1);
+	if (!sub)
+		return (-1);
+	subparse = ft_substr(cmd_t->parsing_v[i], *idx - len - 1, len + 1);
+	if (!subparse)
+		return (-1);
+	val = get_env_by_id(env, sub + 1);
+	if (val)
+	{
+		if (parse_quotes_found_value(cmd_t, &sub, subparse, val->value, i) == -1)
+			return (-1); // Memory error
+		*idx += (ft_strlen(val->value) - (len + 2));
+	}
+	else
+	{
+		if (parse_quotes_not_found(cmd_t, &sub, subparse, i) == -1)
+			return (-1); // Memory error
+		*idx -= (len + 2);
+	}
+	free(subparse);	
 	return (0);
 }
 
@@ -333,127 +574,21 @@ int	parse_quotes(t_cmd_lst *cmd_t, t_list *env)
 {
 	int		i;
 	int		idx;
-	int		rtrn;
-	int		backup;
-	size_t	len;
-	t_list	*val;
-	char	*sub;
-	char	*subparse;
 
-	cmd_t->parsing_v = malloc(sizeof(char *) * (cmd_t->arg_c + 1));
-	if (!cmd_t->parsing_v)
+	if (parse_quotes_init(cmd_t, &i) == -1)
 		return (-1);
-	i = 0;
-	while (i < cmd_t->arg_c)
-	{
-		cmd_t->parsing_v[i] = ft_strdup_char('M', ft_strlen(cmd_t->arg_v[i]));
-		if (!cmd_t->parsing_v[i])
-			return (-1); // Memory error
-		i++;
-	}
-	cmd_t->parsing_v[i] = NULL;
-	i = 0;
-	while (i < cmd_t->arg_c)
-	{
-		parse_input_quotes(cmd_t->arg_v[i], cmd_t->parsing_v[i]);
-		i++;
-	}
-	i = 0;
-	while (i < cmd_t->arg_c)
-	{
-		idx = 0;
-		while (cmd_t->arg_v[i][idx])
-		{
-			if (cmd_t->parsing_v[i][idx] == 'E')
-			{
-				rtrn = remove_char_from_str(cmd_t, &cmd_t->arg_v[i], idx);
-				if (rtrn == -1)
-					return (-1);
-				if (rtrn == 1)
-				{
-					i--;
-					break ;
-				}
-				rtrn = remove_char_from_str(NULL, &cmd_t->parsing_v[i], idx);
-				if (rtrn == -1)
-					return (-1);
-				if (rtrn == 1)
-				{
-					i--;
-					break ;
-				}
-			}
-			else
-				idx++;
-		}
-		i++;
-	}
-	sub = NULL;
-	i = 0;
+	if (parse_quotes_delete(cmd_t, &i, &idx) == -1)
+		return (-1);
 	while (i < cmd_t->arg_c)
 	{
 		idx = 0;
 		while (cmd_t->arg_v[i] && cmd_t->arg_v[i][idx])
 		{
-			if (cmd_t->arg_v[i][idx] == '$' && (is_envchar(cmd_t->arg_v[i][idx + 1], 1) ||
+			if (cmd_t->arg_v[i][idx] == '$' && (is_envchar(cmd_t->arg_v[i][idx + 1], 1) || \
 			cmd_t->arg_v[i][idx + 1] == '?') && cmd_t->parsing_v[i][idx] == 'M')
 			{
-				len = 0;
-				idx++;
-				if (cmd_t->arg_v[i][idx] != '?')
-				{
-					while (cmd_t->parsing_v[i][idx] && \
-						(cmd_t->parsing_v[i][idx] == 'D' || cmd_t->parsing_v[i][idx] == 'M') && \
-						cmd_t->arg_v[i][idx] != '$' && cmd_t->arg_v[i][idx] != '\'' && \
-						cmd_t->arg_v[i][idx] != '\"' && is_envchar(cmd_t->arg_v[i][idx], (len == 0)))
-					{
-						len++;
-						idx++;
-					}
-				}
-				else
-				{
-					len = 1;
-					idx++;
-				}
-				sub = ft_substr(cmd_t->arg_v[i], idx - len - 1, len + 1);
-				if (!sub)
+				if (parse_quotes_loop(cmd_t, env, &idx, i) == -1)
 					return (-1);
-				subparse = ft_substr(cmd_t->parsing_v[i], idx - len - 1, len + 1);
-				if (!subparse)
-					return (-1);
-				val = get_env_by_id(env, sub + 1);
-				if (val)
-				{
-					backup = cmd_t->arg_c;
-					if (replace_sub(cmd_t, &cmd_t->arg_v[i], sub, val->value) != 0)
-						return (-1); // memory error
-					free(sub);
-					if (backup == cmd_t->arg_c)
-					{
-						sub = ft_strdup_char('M', ft_strlen(val->value));
-						if (!sub)
-							return (-1); // memory error
-						if (replace_sub(NULL, &cmd_t->parsing_v[i], subparse, sub) != 0)
-							return (-1); // memory error
-						free(sub);
-					}
-					idx += (ft_strlen(val->value) - (len + 2));
-				}
-				else
-				{
-					backup = cmd_t->arg_c;
-					if (replace_sub(cmd_t, &cmd_t->arg_v[i], sub, "") != 0)
-						return (-1);
-					free(sub);
-					if (backup == cmd_t->arg_c)
-					{
-						if (replace_sub(NULL, &cmd_t->parsing_v[i], subparse, "") != 0)
-							return (-1); // memory error
-					}
-					idx -= (len + 2);
-				}
-				free(subparse);
 			}
 			idx++;
 		}
@@ -461,6 +596,11 @@ int	parse_quotes(t_cmd_lst *cmd_t, t_list *env)
 	}
 	return (0);
 }
+
+
+
+
+
 
 /* Put options into the options_v array */
 int	parse_options(t_cmd_lst *cmd_t)
