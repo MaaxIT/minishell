592f70fd5e87ca0b1e6dfb7a64371920d9574f94
MaaxIT <contact@maaxit.fr>
2022-08-03 14:11:43 +0200

    push debugs

--- src/mem_utils2.c
+++ src/mem_utils2.c
@@ -6,7 +6,7 @@
 /*   By: maxime <maxime@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 00:37:40 by maxime            #+#    #+#             */
-/*   Updated: 2022/08/03 13:43:20 by maxime           ###   ########.fr       */
+/*   Updated: 2022/08/03 14:06:41 by maxime           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ int	get_next_id(t_cmd_lst *cmd_t, int size, char **arr, char *input)
 	int		i;
 	int		j;
 	char	*diff;
+
+	printf("Searching the right id for |%s| in the array:\n", input);
+	for (int c = 0; arr[c]; c++)
+		printf("	%d: |%s|\n", c, arr[c]);
 
 	i = 0;
 	while (i < size)
@@ -32,7 +36,10 @@ int	get_next_id(t_cmd_lst *cmd_t, int size, char **arr, char *input)
 				j++;
 			}
 			if ((size_t)j == ft_strlen(cmd_t->parsing_v[i]))
+			{
+				printf("Found id %d: |%s|\n\n", i, arr[i]);
 				return (i);
+			}
 		}
 		i++;
 	}
