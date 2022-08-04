6779224e479a40cdc0642f8c6451d7f42a895df8
MaaxIT <contact@maaxit.fr>
2022-08-04 18:24:16 +0200

    Fix envp issue

--- src/exec_with_path_utils.c
+++ src/exec_with_path_utils.c
@@ -6,7 +6,7 @@
 /*   By: maxime <maxime@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 19:49:28 by mbennafl          #+#    #+#             */
-/*   Updated: 2022/07/24 15:56:16 by maxime           ###   ########.fr       */
+/*   Updated: 2022/08/04 18:03:16 by maxime           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,7 +96,10 @@ char	**create_envp(t_list *env)
 		tmp = ft_strjoin(env->id, "=");
 		if (!tmp)
 			return (NULL);
-		envp[i] = ft_strjoin(tmp, env->value);
+        if (env->value)
+            envp[i] = ft_strjoin(tmp, env->value);
+        else
+            envp[i] = ft_strjoin(tmp, "");
 		if (!envp[i])
 			return (NULL);
 		free(tmp);
